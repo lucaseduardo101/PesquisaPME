28fcf84eefe2003ce72c0fd42418240ee56bb61d
Leco <lucaseduardo101@alu.ufc.br>
2015-04-14 15:10:04 -0300

    deletando Arquivos

--- consulta1.sql
+++
@@ -1,7 +0,0 @@
-﻿select distinct idade, sexo, diadenascimento, mesdenascimento, anodenascimento, d.numerodeserie ,d.numerodecontrole, d.painel, d.gruporotacional, de.mes, de.ano 
-from pessoa p, domicilio d, domicilio_entrevista de  
-where d.numerodeserie = 1 and d.numerodecontrole = 26000011 and d.painel = 'R' and d.gruporotacional = 1 
-and d.numerodeserie = p.numerodeserie and d.numerodecontrole = p.numerodecontrole and d.painel = p.painel and d.gruporotacional = p.gruporotacional
-and d.numerodeserie = de.numerodeserie and d.numerodecontrole = de.numerodecontrole and d.painel = de.painel and d.gruporotacional = de.gruporotacional
-order by diadenascimento,mesdenascimento, sexo
-
